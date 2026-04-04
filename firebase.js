5c05cf9e4fb39408c28927c3ef5a9f04b30b665f
tinsaebiniyam905-coder <tinsaebiniyam905@gmail.com>
2026-04-04 11:09:47 -0700

    Initialize Firebase and Firestore in firebase.js

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, updateDoc, doc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD2lXdRT34nG2_mmLhgn1Wt7hOT6IF6z7E",
+  authDomain: "election-police-report-sys.firebaseapp.com",
+  projectId: "election-police-report-sys",
+  storageBucket: "election-police-report-sys.firebasestorage.app",
+  messagingSenderId: "832289330129",
+  appId: "1:832289330129:web:7944e2b6e4aa0c82ca86f0"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth, collection, addDoc, getDocs, updateDoc, doc, signInWithEmailAndPassword };
